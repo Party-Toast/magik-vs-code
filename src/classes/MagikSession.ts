224409e0a8f47b181206c8171e294d230cdfe88b
Sytse Walraven <sytse.walraven@keronic.com>
2026-02-27 14:15:17 +0100

    Callback separation + added comments where necessary

--- src/classes/MagikSession.ts
+++ src/classes/MagikSession.ts
@@ -71,41 +71,9 @@ export class MagikSession {
             crlfDelay: Infinity
         })
 
-        lineReader.on('line', line => {
-            const lineWithoutPrompt = line.startsWith('Magik>') ? line.replace('Magik>', '').trimStart() : line
-            this.currentOutput.push(lineWithoutPrompt)
-            if(!this.hideNextOutput) {
-                this.processLine(lineWithoutPrompt)
-            }
-        })
+        lineReader.on('line', this.processSessionLine.bind(this))
 
-        this.process.stdout.on('data', async (chunk: Buffer) => {
-            const lines = chunk.toString().split('\r\n')
-
-            for (const line of lines) {
-                if(line.startsWith('Magik>')) {
-                    this.eventEmitter.emit('magik-ready', this.currentOutput)
-                    this.currentOutput = []
-                    this.hideNextOutput = false
-                    this.appendOutput('\n')
-                    this.cellExecution?.end(true, Date.now())
-                    this.cellExecution = undefined
-                    this.updateStatusBar(false)
-                    break
-                }
-
-                const globalCreationMatch = line.match(Regex.Session.GlobalCreationPrompt)
-                if(globalCreationMatch) {
-                    const shouldCreateGlobal = await vscode.window.showQuickPick(['Yes', 'No'], {
-                        title: globalCreationMatch[1]
-                    })
-                    this.process.stdin.write(`${shouldCreateGlobal === 'Yes' ? 'y' : 'n'}\r\n`)
-                    break
-                }
-            }
-
-
-        })
+        this.process.stdout.on('data', this.processSessionData.bind(this))
     }
 
     async showKillPrompt() {
@@ -202,6 +170,42 @@ export class MagikSession {
         // }), [codeLens.range])
     }
 
+    /**
+     * Checks the Magik session for actionable data.
+     * 
+     * When a Magik prompt is detected, notifies the current @see cellExecution to end and updates the status bar appropriately. 
+     * When a prompt to create a global is detected, it's creation is handled indirectly by a prompt in VSCode instead of in the notebook.
+     * 
+     * In contrast to @see processSessionLine, this does not append the data to the notebook.
+     * 
+     * @param buffer Session byte data
+     */
+    private async processSessionData(buffer: Buffer) {
+        const lines = buffer.toString().split('\r\n')
+
+        for (const line of lines) {
+            if(line.startsWith('Magik>')) {
+                this.eventEmitter.emit('magik-ready', this.currentOutput)
+                this.currentOutput = []
+                this.hideNextOutput = false
+                this.appendOutput('\n')
+                this.cellExecution?.end(true, Date.now())
+                this.cellExecution = undefined
+                this.updateStatusBar(false)
+                break
+            }
+
+            const globalCreationMatch = line.match(Regex.Session.GlobalCreationPrompt)
+            if(globalCreationMatch) {
+                const shouldCreateGlobal = await vscode.window.showQuickPick(['Yes', 'No'], {
+                    title: globalCreationMatch[1]
+                })
+                this.process.stdin.write(`${shouldCreateGlobal === 'Yes' ? 'y' : 'n'}\r\n`)
+                break
+            }
+        }
+    }
+
     async sendSection(range: vscode.Range) {
         const editor = vscode.window.activeTextEditor
         if(!editor) {
@@ -218,6 +222,9 @@ export class MagikSession {
         await this.send(`remex(${exemplarName})`)
     }
 
+    /**
+     * Shows the class browser. If @see classBrowser is not yet set, starts it first.
+     */
     async showClassBrowser() {
         if(!this.classBrowser) {
             await this.send('method_finder.lazy_start?', undefined, true)
@@ -232,11 +239,20 @@ export class MagikSession {
         this.classBrowser.show()
     }
 
+    /**
+     * Sends a string to the Magik session.
+     * 
+     * @param text
+     * @param cell Cell to which the session's response is to be written. If no cell is provided, @see lastExecutedCell is used instead.
+     * @param hideOutput If true, the session's response is not displayed in the notebook.
+     * @returns 
+     */
     async send(text: string, cell?: vscode.NotebookCell, hideOutput = false): Promise<string[]> {
         if(!this.isActive()) {
             vscode.window.showErrorMessage('Session no longer active.')
             return Promise.reject()
         }
+        
         this.updateStatusBar(true)
         this.hideNextOutput = hideOutput
 
@@ -251,38 +267,54 @@ export class MagikSession {
         return once(this.eventEmitter, 'magik-ready')
     }
 
-    private async processLine(line: string) {
+    /**
+     * Cleans up and applies styles to a Magik session output line.
+     * Unless @see hideNextOutput is true, the line appended to the currently executing notebook.
+     * 
+     * @param line
+     */    
+    private async processSessionLine(line: string) {
+        if(line.startsWith('Magik>')) {
+            line = line.replace('Magik>', '').trimStart()
+        }
+
+        this.currentOutput.push(line)
+
+        if(this.hideNextOutput) {
+            return
+        }
+
         const globalCreationMatch = line.match(Regex.Session.GlobalCreationPrompt)
         if(globalCreationMatch) {
             line = line.replace(globalCreationMatch[0], '').trimStart()
         }
 
-        line = line.replaceAll(Regex.Session.Error, error => applyStyle(error, Style.White, Style.RedBackground))
+        line = line
+            .replaceAll(Regex.Session.Error, error => applyStyle(error, Style.White, Style.RedBackground))
+            .replaceAll(Regex.Session.Traceback, traceback => applyStyle(traceback, Style.Red))
+            .replaceAll(Regex.Session.Warning, warning => applyStyle(warning, Style.Black, Style.YellowBackground))
+            .replaceAll(Regex.Session.Global, global => applyStyle(global, Style.Green))
+            .replaceAll(Regex.Session.String, string => applyStyle(string, Style.Yellow))
+            .replaceAll(Regex.Session.TracebackPath, tracebackPath => applyStyle(tracebackPath, Style.Grey))
+            .replaceAll(Regex.Session.Todo, todo => applyStyle(todo, Style.Red))
+            .replaceAll(Regex.Session.Apropos, (_, type: string, name: string, className: string) => {
+                const styledName = name
+                    .replace(/^[\w?!\[\]]*/g, name => applyStyle(name, Style.Yellow))
+                    .replace(' optional ', applyStyle(' optional ', Style.Cyan))
+                    .replace(' gather ', applyStyle(' gather ', Style.Cyan))
 
-        line = line.replaceAll(Regex.Session.Traceback, traceback => applyStyle(traceback, Style.Red))
-
-        line = line.replaceAll(Regex.Session.Warning, warning => applyStyle(warning, Style.Black, Style.YellowBackground))
-
-        line = line.replaceAll(Regex.Session.Global, global => applyStyle(global, Style.Green))
-
-        line = line.replaceAll(Regex.Session.String, string => applyStyle(string, Style.Yellow))
-
-        line = line.replaceAll(Regex.Session.Apropos, (_, type: string, name: string, className: string) => {
-            const styledName = name
-                .replace(/^[\w?!\[\]]*/g, name => applyStyle(name, Style.Yellow))
-                .replace(' optional ', applyStyle(' optional ', Style.Cyan))
-                .replace(' gather ', applyStyle(' gather ', Style.Cyan))
-
-            return `${applyStyle(type, type === 'CORRUPT' ? Style.Red : Style.Blue)} ${styledName} ${applyStyle('in', Style.Grey)} ${applyStyle(className, Style.Green)}`
-        })
-
-        line = line.replaceAll(Regex.Session.TracebackPath, tracebackPath => applyStyle(tracebackPath, Style.Grey))
-
-        line = line.replaceAll(Regex.Session.Todo, todo => applyStyle(todo, Style.Red))
+                return `${applyStyle(type, type === 'CORRUPT' ? Style.Red : Style.Blue)} ${styledName} ${applyStyle('in', Style.Grey)} ${applyStyle(className, Style.Green)}`
+            })
 
         this.appendOutput(line === '' ? ' ' : line)
     }
 
+    /**
+     * Append a line to the current @see cellExecution. 
+     * If no cell is currently executing, a temporary execution is created in the last executed cell.
+     * 
+     * @param line 
+     */
     private appendOutput(line: string) {
         if(!this.cellExecution) {
             const tempExecution  = magikNotebookController.createNotebookCellExecution(this.lastExecutedCell!)
