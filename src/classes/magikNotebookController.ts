4292dc3330bb0e1f19d315734bad78e1f32d33a1
Sytse Walraven <sytse.walraven@keronic.com>
2026-04-12 00:25:33 +0200

    Notebook (de)serialization (#33)
    * Notebook (de)serialization
    
    * Renamed magikNotebookController file
    
    * Touch magikNotebookController to track rename on remote
    
    * Rename MagikNotebookController to magikNotebookController

---
+++ src/classes/magikNotebookController.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode'
+import { config, magikSession } from '../extension'
+
+export const magikNotebookController = vscode.notebooks.createNotebookController('magik-notebook-kernel', 'magik-notebook', "Magik Notebook Kernel")
+magikNotebookController.executeHandler = async (cells: vscode.NotebookCell[], notebook: vscode.NotebookDocument, controller: vscode.NotebookController) => {
+	for(const cell of cells) {
+		await magikSession.send(cell.document.getText(), cell)
+
+		if(!config.get<Boolean>('createCellAfterExecution') || cell.index !== notebook.cellCount - 1) {
+			continue
+		}
+
+		const newCellText = config.get<Boolean>('copyContentOnCellCreation') ? cell.document.getText() : ''
+		const newCell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, newCellText, 'magik')
+		const edit = new vscode.WorkspaceEdit()
+		edit.set(notebook.uri, [
+			vscode.NotebookEdit.insertCells(notebook.cellCount, [newCell])
+		])
+		await vscode.workspace.applyEdit(edit)
+		await vscode.commands.executeCommand('notebook.focusBottom')
+		await vscode.commands.executeCommand('notebook.cell.edit')
+	}
+}
